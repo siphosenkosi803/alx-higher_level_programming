7133f63b5e5f741d364b84fc5bf4c68fabb4caa9
Siphosenkosi Mlotshwa <sibusisoradebe943@yahoo.com>
2022-11-29 19:04:40 -0800

    6ix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -6,3 +6,10 @@ for i in range(10):
         elif i == 8 and j == 9:
             print("{}{}".format(i, j))
             break
+#!/usr/bin/python3
+for a1 in range(0, 10):
+    for a2 in range(a1 + 1, 10):
+        if a1 == 8 and a2 == 9:
+            print("{}{}".format(a1, a2))
+        else:
+            print("{}{}".format(a1, a2), end=", ")
