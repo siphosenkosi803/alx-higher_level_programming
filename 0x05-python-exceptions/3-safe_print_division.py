25b1066d9b3bc586b1e507a3b24f248da379b257
Siphosenkosi Mlotshwa <sibusisoradebe943@yahoo.com>
2022-12-19 06:58:19 -0800

    z

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+
+    """
+
+    Divides two integers and print the result
+
+    """
+
+    try:
+
+        result = a/b
+
+    except:
+
+        result = None
+
+    finally:
+
+        print('Inside result: {}'.format(result))
+
+        return result
