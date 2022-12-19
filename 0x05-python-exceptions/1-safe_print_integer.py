d306f07b74d4b44c8d2df79f8ec785bf3543bb83
Siphosenkosi Mlotshwa <sibusisoradebe943@yahoo.com>
2022-12-19 06:49:55 -0800

    g

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """
+
+    Prints integer with format flags
+
+    """
+    try:
+        print('{:d}'.format(value))
+    except:
+        return False
+    return True
