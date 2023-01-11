711a5f939f227eae7de2715e1799000d831cd6f2
Siphosenkosi Mlotshwa <sibusisoradebe943@yahoo.com>
2023-01-11 00:42:47 -0800

    test commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""ReadFile module"""
+
+
+def read_file(filename=""):
+    """Reads a text file and prints it out
+
+    Args:
+        filename (str): the filename of the text file
+
+    """
+
+    with open(filename, encoding="UTF-8") as f:
+        string = f.read()
+        print(string, end="")
