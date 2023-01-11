90cbb4f38df372716416b4fd6b6025dc97fc25fc
Siphosenkosi Mlotshwa <sibusisoradebe943@yahoo.com>
2023-01-11 03:30:54 -0800

    commit stats

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""reads stdin line by line"""
+def number_of_lines(filename=REPLACEquot;REPLACEquot;):
+    """define line"""
+    return (0)
+print(def number_of_lines(filename=REPLACEquot;REPLACEquot;))
