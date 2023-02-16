11017d6aa3caa176dc3dce7f711b9c0e51b29e8a
Siphosenkosi Mlotshwa <sibusisoradebe943@yahoo.com>
2023-02-16 07:41:20 -0800

    commit first task

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Grant privileges in my sql server
+-- Lists all privileges for the user user_0d_1
+SHOW GRANTS FOR user_0d_1@localhost;
+-- Lists all privileges of the user user_0d_2
+SHOW GRANTS FOR user_0d_2@localhost;
