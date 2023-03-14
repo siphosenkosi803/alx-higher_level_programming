d65e96a1cff6f46c56b8f87017aa7eeff15b45b0
siphosenkosi803 <sibusisoradebe943@yahoo.com>
2023-03-14 04:36:37 -0700

    commit task 9

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// prints the number of arguments already printed and the new
+// argument value
+let index = 0;
+exports.logMe = function (item) {
+  console.log(`${index}: ${item}`);
+  index++;
+};
+
