d307194be18c60059a0f0112b65291ccf755baab
siphosenkosi803 <sibusisoradebe943@yahoo.com>
2023-03-14 04:10:47 -0700

    commit task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// class that defines a Rectangle
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let j = 0; j < this.height; j++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log('');
+    }
+  }
+}
+module.exports = Rectangle;
+
