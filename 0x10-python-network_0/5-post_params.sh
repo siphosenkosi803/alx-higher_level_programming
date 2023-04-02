dbfd17c594a9507adb16e5d38c14491a13e28c4b
siphosenkosi803 <110998052+siphosenkosi803@users.noreply.github.com>
2023-04-02 11:46:46 +0200

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# send a POST request to the passed URL using curl, and display the body of the response
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
-
+# Sends a POST request
+curl "$1" -sX POST -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD"
