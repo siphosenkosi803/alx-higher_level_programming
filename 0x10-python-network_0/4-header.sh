f0d3964930c4159eb28db6a9843b74ae73ea32c9
siphosenkosi803 <110998052+siphosenkosi803@users.noreply.github.com>
2023-04-02 11:44:44 +0200

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# send a GET request to the URL using curl, and display the body of the response
-curl -sH "X-School-User-Id: 98" "$1"
-
+# sends a GET request and display the body
+curl "$1" -sX GET -H "X-HolbertonSchool-User-Id: 98"
