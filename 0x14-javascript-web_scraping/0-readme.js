80bc7a8d0d53241d3efa100996d39d4c9db5dc35
siphosenkosi803 <110998052+siphosenkosi803@users.noreply.github.com>
2023-04-25 16:08:56 +0200

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-const _f_s_ = require('fs');
+const _f_s_ = require ('fs');
 const _read_f = process.argv[2];
 
-_f_s_.readFile(_read_f, 'utf8', (error, _f_Content_) => {
+_f_s_.readFile (_read_f, 'utf8', (error, _f_Content_) => {
   if (error) {
-    console.error(error);
+  console.error (error);
   } else {
-    console.log(_f_Content_);
+    console.log (_f_Content_);
   }
 });
-
