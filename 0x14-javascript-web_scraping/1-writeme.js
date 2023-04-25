ff7e1a52a593bea237a987206d434cc5a20539e8
siphosenkosi803 <110998052+siphosenkosi803@users.noreply.github.com>
2023-04-25 20:36:37 +0200

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
-// writes a string to a file
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
-  if (err) {
-    console.log(err);
-  }
-}
-);
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
+});
